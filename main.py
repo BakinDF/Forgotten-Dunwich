a467af547d71a3fdc44817a3452e05e6404694a6
MrEmgin <denis.bakin5@gmail.com>
2020-01-09 21:40:53 +0300

    wave algorithm is being done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from copy import deepcopy
 
 pygame.init()
 size = width, height = 1100, 700
@@ -16,6 +17,7 @@ enemy_group = pygame.sprite.Group()
 text = None
 door = None
 
+delt_x, delt_y = 0, 0
 # money, health
 player_params = []
 
@@ -358,8 +360,11 @@ class Camera:
             pass
 
     def update(self, target):
+        global delt_x, delt_y
         self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
         self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
+        delt_x -= self.dx
+        delt_y -= self.dy
 
 
 class Goblin(pygame.sprite.Sprite):
@@ -768,6 +773,68 @@ def check_active_zones(obj):
             return
     door = None
     text = None
+
+
+def find_path(path, x1, y1, x2, y2):
+    file = open(path, mode='r', encoding='utf-8')
+    data = [list(i[:-1]) for i in file.readlines()]
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j] in 'wmb':
+                data[i][j] = -1
+            else:
+                data[i][j] = 0
+    a = [print(i) for i in data]
+    lest = deepcopy(data)
+    lest[x1][y1] = 1
+    lest[x2][y2] = 0
+    wave = True
+    height = len(data)
+    width = len(data[0])
+    while wave and lest[x2][y2] == 0:
+        wave = False
+        for i in range(height - 1):
+            for j in range(width - 1):
+                if lest[i][j] not in [0, -1]:
+                    if 0 <= i - 1 < height:
+                        if lest[i - 1][j] == 0:
+                            lest[i - 1][j] = lest[i][j] + 1
+                            wave = True
+                    if 0 <= i + 1 < height:
+                        if lest[i + 1][j] == 0:
+                            lest[i + 1][j] = lest[i][j] + 1
+                            wave = True
+                    if 0 <= j - 1 < width:
+                        if lest[i][j - 1] == 0:
+                            lest[i][j - 1] = lest[i][j] + 1
+                            wave = True
+                    if 0 <= j + 1 < width:
+                        if lest[i][j + 1] == 0:
+                            lest[i][j + 1] = lest[i][j] + 1
+                            wave = True
+    path_lest = []
+    if lest[x2][y2] != 0:
+        x, y = x2, y2
+        path_lest.append((x2, y2))
+        while lest[x][y] != 1:
+            if 0 <= x - 1 < height:
+                if lest[x - 1][y] == lest[x][y] - 1:
+                    path_lest.append((x - 1, y))
+                    x -= 1
+            if 0 <= x + 1 < height:
+                if lest[x + 1][y] == lest[x][y] - 1:
+                    path_lest.append((x + 1, y))
+                    x += 1
+            if 0 <= y - 1 < width:
+                if lest[x][y - 1] == lest[x][y] - 1:
+                    path_lest.append((x, y - 1))
+                    y -= 1
+            if 0 <= y + 1 < width:
+                if lest[x][y + 1] == lest[x][y] - 1:
+                    path_lest.append((x, y + 1))
+                    y += 1
+    file.close()
+    return path_lest[::-1]
 
 
 def create_col_rect(obj):
